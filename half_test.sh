ec05c0a71ec5d8e063c9dd58c10acb37d8e72c05
NeuralOutlet <IrrationalEntertainment@Live.co.uk>
2019-12-29 09:45:14 +0000

    Including the harmonic mean aswell as arithmetic.

--- half_test.sh
+++ half_test.sh
@@ -10,18 +10,31 @@ then
 	exit
 fi
 
+function calc()
+{
+	echo $1 | bc -l
+}
+
 for i in {4..16}
 do
 	length=${i}
 	block=$(( ${i} / 2 ))
 
-	sum=0
+	asum=0
+	hsum=0
+	hoffset=0 # a zero count of the results
 	for j in $(seq 1 $1)
 	do
 		result=`sbcl --script main.lisp $length $block`
-		sum=$(( $sum + $result ))
-		echo -ne "  [n=$i, i=$j]: $sum + ${result}            \r"
+		asum=`calc "$asum + $result"`
+		if ! [ "$result" -eq "0" ]; then
+			hsum=`calc "$hsum + 1/${result}"`
+		else
+			hoffset=$(( hoffset + 1 ))
+		fi
+		echo -ne "  [n=$i, i=$j]: $asum + ${result}            \r"
 	done
-	output=$(( sum / ${1} ))
-	echo -ne "Average (of $1) for size $i: ${output} cycles\n"
+	output=`calc "scale=2; ${asum}/${1}"`
+	houtput=`calc "scale=2; (${1} - ${hoffset})/${hsum}"`
+	echo -ne "$1 runs with genome length $i: \tArithmetic: ${output} cycles, \t Harmonic: ${houtput} cycles. \t Instant solutions: ${hoffset}\n"
 done
